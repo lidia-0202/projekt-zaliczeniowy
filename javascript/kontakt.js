eb9139a0d01c92959e5a4e5034ed31891927eb06
Lidia <lidia@email.com>
2018-01-15 20:14:24 +0100

    korekta formularza

--- javascript/kontakt.js
+++ javascript/kontakt.js
@@ -1,7 +1,6 @@
 function validateForm() {
     var imie = $('#name').val();
     var email = $('#email').val();
-    if (imie !== '' && email !== '') {
+    if (imie !== '' && email !== '' && email.search('@') >=1 ){
         alert ("Dziękuję za poprawne wypełnienie formularza");   
     };
-};
